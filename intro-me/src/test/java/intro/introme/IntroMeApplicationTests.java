4af4a748ee70f85cd7b56f4c668a9cefa4f3d356
0JUU <1754185@hanmail.net>
2023-08-27 21:27:38 +0900

    Initial commit

---
+++ intro-me/src/test/java/intro/introme/IntroMeApplicationTests.java
@@ -0,0 +1,15 @@
+package intro.introme;
+
+import net.bytebuddy.pool.TypePool;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
